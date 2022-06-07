ba97a25c368de75303c10cefc9894c3c2f5efff2
Javier Núñez Cordero <javierignacio.nunez@gmail.com>
2022-06-07 17:00:10 -0400

    se agregan feriados y vacaciones, se cambia el color

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const countdown = ()=>{
+const countdown1 = ()=>{
     const countDate = new Date('June 21, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
@@ -14,11 +14,189 @@ const countdown = ()=>{
     const textSecond = Math.floor((gap % minute)/second);
     
 
-    document.querySelector(".day").innerText = textDay;
-    document.querySelector(".hour").innerText = textHour;
-    document.querySelector(".minute").innerText = textMinute;
-    document.querySelector(".second").innerText = textSecond;
+    document.querySelector(".day-1").innerText = textDay;
+    document.querySelector(".hour-1").innerText = textHour;
+    document.querySelector(".minute-1").innerText = textMinute;
+    document.querySelector(".second-1").innerText = textSecond;
 }
 
+const countdown2 = ()=>{
+    const countDate = new Date('June 27, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
 
-setInterval(countdown, 1000);
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-2").innerText = textDay;
+    document.querySelector(".hour-2").innerText = textHour;
+    document.querySelector(".minute-2").innerText = textMinute;
+    document.querySelector(".second-2").innerText = textSecond;
+}
+
+const countdown3 = ()=>{
+    const countDate = new Date('August 15, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-3").innerText = textDay;
+    document.querySelector(".hour-3").innerText = textHour;
+    document.querySelector(".minute-3").innerText = textMinute;
+    document.querySelector(".second-3").innerText = textSecond;
+}
+const countdown4 = ()=>{
+    const countDate = new Date('September 19, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-4").innerText = textDay;
+    document.querySelector(".hour-4").innerText = textHour;
+    document.querySelector(".minute-4").innerText = textMinute;
+    document.querySelector(".second-4").innerText = textSecond;
+}
+const countdown5 = ()=>{
+    const countDate = new Date('October 10, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-5").innerText = textDay;
+    document.querySelector(".hour-5").innerText = textHour;
+    document.querySelector(".minute-5").innerText = textMinute;
+    document.querySelector(".second-5").innerText = textSecond;
+}
+const countdown6 = ()=>{
+    const countDate = new Date('October 31, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-6").innerText = textDay;
+    document.querySelector(".hour-6").innerText = textHour;
+    document.querySelector(".minute-6").innerText = textMinute;
+    document.querySelector(".second-6").innerText = textSecond;
+}
+const countdown7 = ()=>{
+    const countDate = new Date('November 1, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-7").innerText = textDay;
+    document.querySelector(".hour-7").innerText = textHour;
+    document.querySelector(".minute-7").innerText = textMinute;
+    document.querySelector(".second-7").innerText = textSecond;
+}
+const countdown8 = ()=>{
+    const countDate = new Date('December 8, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-8").innerText = textDay;
+    document.querySelector(".hour-8").innerText = textHour;
+    document.querySelector(".minute-8").innerText = textMinute;
+    document.querySelector(".second-8").innerText = textSecond;
+}
+
+const countdown9 = ()=>{
+    const countDate = new Date('July 6, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/hour);
+    const textMinute = Math.floor((gap % hour)/minute);
+    const textSecond = Math.floor((gap % minute)/second);
+    
+
+    document.querySelector(".day-9").innerText = textDay;
+    document.querySelector(".hour-9").innerText = textHour;
+    document.querySelector(".minute-9").innerText = textMinute;
+    document.querySelector(".second-9").innerText = textSecond;
+}
+
+setInterval(countdown1, 1000);
+setInterval(countdown2, 1000);
+setInterval(countdown3, 1000);
+setInterval(countdown4, 1000);
+setInterval(countdown5, 1000);
+setInterval(countdown6, 1000);
+setInterval(countdown7, 1000);
+setInterval(countdown8, 1000);
+setInterval(countdown9, 1000);
